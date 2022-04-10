4417ffd10bf59cc1c05c9d9d970524cb9def2221
Dawkon99 <Dawkon99@gmail.com>
2022-04-10 15:30:52 +0200

    Fix text in data from Twitter

--- TwitterData/TwitterApi.py
+++ TwitterData/TwitterApi.py
@@ -18,7 +18,7 @@ class TwitterApi:
     def get_data_from_twitter(self):
         max_results = 100
         max_tweets = 3300
-        tweet_field = 'created_at'
+        tweet_field = 'created_at,context_annotations,entities'
         query = {
             'max_results': max_results,
             'return_json': True,
@@ -26,16 +26,28 @@ class TwitterApi:
         }
         for page in self.twitter_pages:
             user_id = self.api.get_user(username=page, return_json=True)['data']['id']
+            if page in list(self.data.keys()):
+                query.update({
+                    'since_id': max(self.data[page], key=lambda x: int(x['id']))['id']
+                })
+            else:
+                query.update({
+                    'since_id': None
+                })
             current_tweets_data = []
             query.update({'pagination_token': None})
             while len(current_tweets_data) <= max_tweets:
                 current_tweets = self.api.get_timelines(user_id, **query)
-                print('Pobrano {} tweetów z {}'.format(len(current_tweets), page))
-                current_tweets_data.extend(current_tweets['data'])
+                print('Pobrano {} tweetów z {}'.format(current_tweets['meta']['result_count'], page))
+                if 'data' in list(current_tweets.keys()):
+                    current_tweets_data.extend(current_tweets['data'])
                 if 'next_token' not in current_tweets['meta'].keys():
                     break
                 query.update({'pagination_token': current_tweets['meta']['next_token']})
-            self.data.update({page: current_tweets_data})
+            if page in list(self.data.keys()):
+                self.data[page].extend(current_tweets_data)
+            else:
+                self.data.update({page: current_tweets_data})
 
     def save_data_to_txt_files(self):
         try:
@@ -43,13 +55,16 @@ class TwitterApi:
         except FileExistsError:
             pass
         for key, data in self.data.items():
-            with open('{}/{}.txt'.format(self.dir_name, key), 'w') as f:
-                f.write(json.dumps(data))
+            with open('{}/{}.txt'.format(self.dir_name, key), 'w', encoding='utf8') as f:
+                f.write(json.dumps(data, ensure_ascii=False))
 
     def load_data_from_files(self):
         for page in self.twitter_pages:
-            with open('{}/{}.txt'.format(self.dir_name, page), 'r') as f:
-                self.data.update({page: json.loads(f.read())})
+            try:
+                with open('{}/{}.txt'.format(self.dir_name, page), 'r', encoding='utf8') as f:
+                    self.data.update({page: json.loads(f.read())})
+            except FileNotFoundError:
+                pass
 
     def summary(self):
         for key, values in self.data.items():
